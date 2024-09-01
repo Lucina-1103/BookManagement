deb580d494d01bbc236d48e885000032be85ea8e
Lucina <lucina.tech@gmail.com>
2024-09-01 23:39:27 +0900

    fix:パッケージ宣言とDB周りの整備

--- postgres/sql/01_create_db.sql
+++ postgres/sql/01_create_db.sql
@@ -1,14 +1,16 @@
 -- データベースの作成
-CREATE DATABASE helloworld;
+CREATE DATABASE book_management;
 
 -- データベースの切り替え
-\c helloworld
+\c book_management
 
 -- テーブルの作成
-CREATE TABLE helloworld (
-    id INTEGER,
+CREATE TABLE book (
+    uuid uuid,
     name VARCHAR
 );
 
 -- 初期データの投入
-INSERT INTO helloworld VALUES (1, 'Lucina');
+INSERT INTO book VALUES ('6b502a7f-a01a-f687-5d1e-827c8d61805a', 'サンプル書籍名１');
+INSERT INTO book VALUES ('a318c3f3-c55a-471b-e461-fdb8a7f6d5eb', 'サンプル書籍名２');
+INSERT INTO book VALUES ('246f4711-f062-f613-5794-f86da8ac22c1', 'サンプル書籍名３');
