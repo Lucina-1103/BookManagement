33bb94fcd8fb4f46a7f64574c63a7b36d92c9181
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-09-23 00:09:54 +0900

    add:Bookサービス一式を追加 (#7)

---
+++ src/main/java/bookmanagement/entity/BookEntity.java
@@ -0,0 +1,23 @@
+package bookmanagement.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Book")
+@Data
+@NoArgsConstructor
+public class BookEntity {
+    @Id
+    @Column(name = "uuid")
+    private UUID uuid;
+
+    @Column(name = "title")
+    private String title;
+}
