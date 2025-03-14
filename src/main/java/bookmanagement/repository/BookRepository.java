cff0f8c333ca6d6fbc1b4b6edfddf9e4cc39f609
Lucina <lucina.tech@gmail.com>
2025-03-15 08:22:01 +0900

    add:書籍の新規登録機能を追加

--- src/main/java/bookmanagement/repository/BookRepository.java
+++ src/main/java/bookmanagement/repository/BookRepository.java
@@ -1,8 +1,12 @@
 package bookmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import bookmanagement.entity.BookEntity;
 
 public interface BookRepository extends JpaRepository<BookEntity, String> {
+
+    @Query("SELECT MAX(sortOrder) FROM BookEntity")
+    Integer findMaxSortOrder();
 }
--- src/main/java/bookmanagement/service/BookService.java
+++ src/main/java/bookmanagement/service/BookService.java
@@ -4,7 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import bookmanagement.entity.BookEntity;
+import bookmanagement.form.BookForm;
+
 
 public interface BookService {
     public Page<BookEntity> getBookEntityPage(Pageable pageable);
+
+    public BookEntity saveBookEntity(BookForm bookForm);
 }
