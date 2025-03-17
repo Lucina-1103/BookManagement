5da46caab99ece5b9225e073adece10ec2b5dcf2
Lucina <lucina.tech@gmail.com>
2025-03-17 16:49:09 +0900

    add:書籍情報の照会ページを追加

--- src/main/java/bookmanagement/repository/BookRepository.java
+++ src/main/java/bookmanagement/repository/BookRepository.java
@@ -1,11 +1,15 @@
 package bookmanagement.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import bookmanagement.entity.BookEntity;
 
 public interface BookRepository extends JpaRepository<BookEntity, String> {
+
+    BookEntity findByUuid(UUID uuid);
 
     @Query("SELECT MAX(sortOrder) FROM BookEntity")
     Integer findMaxSortOrder();
